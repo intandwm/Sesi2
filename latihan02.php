5d673d744958ae61ec0035d818e4199f0767f685
intandwm <108415153+intandwm@users.noreply.github.com>
2023-03-25 09:27:42 +0800

    Add files via upload

---
+++ latihan02.php
@@ -0,0 +1,12 @@
+<?php
+    $angka = 3;
+
+    echo "Nilai Angka: <strong>".$angka. "</strong>";
+
+    if(($angka > 4) and (angka<9) ){
+        //kondisi true
+        echo "<br>Nilai Angka Lebih dari 4";
+    }else{
+        //kondisi false
+        echo "<br>Nilai Angka kurang dari sama dengan 4";
+    }
